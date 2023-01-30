438050b94b11d5725c5207ddf22f047eb34e1985
yuro-odoo <yuro@odoo.com>
2023-01-30 14:01:54 -0800

    [ADD]added lost file back

---
+++ pac_crm/static/src/js/rental_config.js
@@ -0,0 +1,28 @@
+odoo.define('pac_crm.rental_config', function (require) {
+    var core = require('web.core');
+    var ProductConfiguratorWidget = require('sale_renting.rental_configurator');
+    
+    var _t = core._t;
+    
+
+    ProductConfiguratorWidget.include({
+         /**
+         * Opens the rental configurator in 'edit' mode.
+         *
+         * @override
+         * @private
+         */
+        _onEditLineConfiguration: function () {
+            if (this.recordData.is_rental) {// and in rental app ? (this.nodeOptions.rent)
+                return
+            } else {
+                return
+            }
+        },
+
+        _checkIfRentable: function (productId, dataPointID) {
+            return
+        }
+    })
+    return ProductConfiguratorWidget;
+})
